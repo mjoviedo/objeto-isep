aab05c70d9471c56ab82f7d438fb8c7289ef5491
marcuse99 <marcuse99@tuta.io>
2022-12-15 18:46:39 -0300

    probando botones para ocultar

--- funciones.js
+++ funciones.js
@@ -5,3 +5,16 @@ function openNav() {
 function closeNav() {
   document.getElementById("mySidenav").style.width = "0";
 }
+
+
+function myFunction() {
+  var x = document.getElementById("nota");
+  if (x.style.display === "none") {
+    x.style.display = "block";
+  } else {
+    x.style.display = "none";
+  }
+}
+
+
+</script>
