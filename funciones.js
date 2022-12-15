7ecee765dc9e56211f7e8d6042f18b11bb1b4672
marcuse99 <marcuse99@tuta.io>
2022-12-15 18:50:52 -0300

    correccion menu lateral

--- funciones.js
+++ funciones.js
@@ -5,16 +5,3 @@ function openNav() {
 function closeNav() {
   document.getElementById("mySidenav").style.width = "0";
 }
-
-
-function myFunction() {
-  var x = document.getElementById("nota");
-  if (x.style.display === "none") {
-    x.style.display = "block";
-  } else {
-    x.style.display = "none";
-  }
-}
-
-
-</script>
